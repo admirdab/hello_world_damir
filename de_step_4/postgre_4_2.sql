2d579e28d7b650cf46ebbc5e8ebd83fbf03b7292
admirdab <dam1rbadmaev96@gmail.com>
2025-06-21 09:45:35 +0300

    add foreign_key for users_audit and correct log function

--- de_step_4/postgre_4_2.sql
+++ de_step_4/postgre_4_2.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
 --Создание таблицы users_audit для логгирования изменений полей таблицы user
 CREATE TABLE users_audit (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
+    user_id INTEGER REFERENCES users(id),
     changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     changed_by TEXT,
     field_changed TEXT,
@@ -21,18 +21,18 @@ create or replace function log_user_update()
 returns trigger as $$
 begin
 	if old.name is distinct from new.name  then
-	insert into users_audit (user_id, field_changed, old_value, new_value)
-	values(new.id, 'name', old.name, new.name);
+	insert into users_audit (user_id, changed_by, field_changed, old_value, new_value)
+	values(old.id, current_user, 'name', old.name, new.name);
 	end if;
 
 	if old.email is distinct from new.email then
-	insert into users_audit (user_id, field_changed, old_value, new_value)
-	values(new.id, 'email', old.email, new.email);
+	insert into users_audit (user_id, changed_by, field_changed, old_value, new_value)
+	values(old.id, current_user, 'email', old.email, new.email);
 	end if;
 
 	if old.role is distinct from new.role  then
-	insert into users_audit (user_id, field_changed, old_value, new_value)
-	values(new.id, 'role', old.role, new.role);
+	insert into users_audit (user_id, changed_by, field_changed, old_value, new_value)
+	values(old.id, current_user, 'role', old.role, new.role);
 	end if;	
 return new;			   
 end;
@@ -47,7 +47,7 @@ execute function log_user_update();
 --установка расширения pg_cron
 create extension if not exists pg_cron;
 --проверка установки
-select * from pg_extension
+select * from pg_extension;
 show cron.database_name;
 --функция для экспорта данных из users_audit на текущую дату
 create or replace function export_current_date_user_audit_tocsv()
